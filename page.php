fcb62825182339acf94015dcb0b20f8817730e92
Thery <j.thery2311@outlook.fr>
2019-02-18 16:13:03 +0100

    add content page on page.php

--- page.php
+++ page.php
@@ -2,38 +2,76 @@
 get_header();
 ?>
 
-<div class="container">
-<?php
-if ( have_posts() ):
-    while (have_posts()):
-        the_post();
-?>
-    <article>
-        <div class="row">
-            <div class="col-sm">
-                <h1><?php the_title(); ?></h1>
+    <!--test-->
+    <div class="container-institut">
+        <div class="container-img">
+            <img src="ressources/images/test-intro-img.jpg" alt="#">
+        </div>
+        <div class="container-title-1">
+            <p>Institut de beauté</p>
+            <div class="container-txt">
+                <p>Prendre soin de soi !
+
+                    Dans un monde où nous ne trouvons plus le temps de prendre soin de nous, l'institut
+                    BeautyByMarine vient nous rappeler qu'il est important de s'accorder une pause. Une
+                    pause bien
+                    méritée dans nos journées remplies de nos tâches quotidiennes, professionnelles ou
+                    encore
+                    imprévues. Offrez-vous cette pause, poussez les portes de l'institut pour un instant de
+                    relaxation et de bien-être ! L'équipe vous apporte les meilleurs conseils personnalisés
+                </p>
             </div>
         </div>
-        <div class="row">
-            <div class="col-sm article-illustration">
-<?php
-        if ( has_post_thumbnail() ):
-            the_post_thumbnail( 'full' );
-        endif;
-?>
+    </div>
+    <!--end test-->
+    <!--prestations-->
+    <div class="place-prestations" id="container-prestations">
+        <div class="prestations">
+            <div>
+                <a href="epilation.html">Epilations</a>
             </div>
         </div>
-        <div class="row">
-            <div class="col-sm">
-                <div><?php the_content(); ?></div>
+        <div class="prestations">
+            <div>
+                <a href="soins-visages.html">Soins visages</a>
             </div>
         </div>
-    </article>
-<?php
-    endwhile;
-endif;
-?>
-</div>
+        <div class="prestations">
+            <div>
+                <a href="soins-corps.html">Soins corps</a>
+            </div>
+        </div>
+    </div>
+    <div class="place-prestations-1">
+        <div class="prestations">
+            <div>
+                <a href="onglerie.html">Onglerie</a>
+            </div>
+        </div>
+        <div class="prestations" id="prestations-media">
+            <div>
+                <a href="maquillage.html">Maquillage</a>
+            </div>
+        </div>
+    </div>
+    <!--end prestations-->
+    <!--carte cadeau-->
+    <!--carte cadeau-->
+    <div class="container-cadeau">
+        <div class="cadeau">
+            <div>
+                <img src="http://www.millesimevoyages.be/Pics/cadeaux.png" alt="#" style="height: 350px; width: 100%;">
+            </div>
+            <div class="p-cadeau">
+                <p>Un anniversaire, une fête ou juste l'envie de faire plaisir ?</p>
+                <p>Offrez du Bien-être à ceux que vous aimez !</p>
+                <div class="lien-cadeau">
+                    <a href="#">voir plus</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!--end carte cadeau-->
 
 <?php
 get_footer();
